69ee189e442a1c3956a54d73b59dbda5816d048d
ferricode <ferided88@gmail.com>
2023-03-15 17:09:17 +0200

    Sortin by title and date added.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const databaseInit = require('./config/databaseInit');
 const seedDbFromJson = require('./services/databaseService');
-//const json = require('koa-json');
 const newsService = require('./services/newsService');
 
 //If there is no need of seed uncomment databaseInit() and comment seedDbFromJson();
@@ -11,11 +10,10 @@ const newsService = require('./services/newsService');
 //seedDbFromJson();
 databaseInit();
 
-
 const app = new Koa();
 const router = new Router();
 
-router.get('/news', async (ctx) => {
+router.get('/fenews/filter', async (ctx) => {
     const title = ctx.query.title;
     const from = ctx.query.from;
     const to = ctx.query.to;
@@ -27,14 +25,17 @@ router.get('/news', async (ctx) => {
         if (title) {
             news = news.filter(news => news.title.toLowerCase().includes(title.toLowerCase()));
         }
+        //Filter by start date
         if (from) {
             const fromCompare = new Date(from).toISOString().split('T')[0];
             news = news.filter(news => new Date(news.publishedAt).toISOString().split('T')[0] >= fromCompare);
         }
+        //Filer by end Date
         if (to) {
             const toCompare = new Date(to).toISOString().split('T')[0];
             news = news.filter(news => new Date(news.publishedAt).toISOString().split('T')[0] <= toCompare);
         }
+        //Filter by exact date
         if (exactDate) {
             const exactDateCompare = new Date(exactDate).toISOString().split('T')[0];
             news = news.filter(news => new Date(news.publishedAt).toISOString().split('T')[0] === exactDateCompare);
@@ -49,23 +50,54 @@ router.get('/news', async (ctx) => {
     ctx.body = news;
 
 });
-router.get('/', async ctx => {
+router.get('/fenews/sort', async (ctx) => {
+    const titleSort = ctx.query.title;
+    const date = ctx.query.date;
+    let news = await newsService.getAll();
+
     try {
-        const news = await newsService.getAll();
-        ctx.body = news;
+        //Sort by title
+        if (titleSort == 'asc') {
+            news = news.sort((a, b) => {
+                const aFirstWord = a.titleSort.split(/[\s]+/, 1)[0].replace(/^['‘"]+|['‘"]+$/g, '');
+                const bFirstWord = b.titleSort.split(/[\s,]+/, 1)[0].replace(/^['‘"]+|['‘"]+$/g, '');
+                return aFirstWord.toLowerCase() < bFirstWord.toLowerCase()
+                    ? -1
+                    : 1;
+            });
+        } else if (titleSort == 'desc') {
+            news = news.sort((a, b) => {
+                const aFirstWord = a.titleSort.split(/[\s]+/, 1)[0].replace(/^['‘"]+|['‘"]+$/g, '');
+                const bFirstWord = b.titleSort.split(/[\s,]+/, 1)[0].replace(/^['‘"]+|['‘"]+$/g, '');
+                return aFirstWord.toLowerCase() > bFirstWord.toLowerCase()
+                    ? -1
+                    : 1;
+            });
+
+        }
+        //Sort by date
+        if (date == "asc") {
+            news = news.sort((a, b) => {
+                return a.publishedAt < b.publishedAt
+                    ? -1
+                    : 1;
+            });
+        } else if (date == 'desc') {
+            news = news.sort((a, b) => {
+                return a.publishedAt < b.publishedAt
+                    ? 1
+                    : -1;
+            });
+        }
     } catch (err) {
         ctx.status = 500;
         ctx.body = { message: err.message };
     }
+
+    ctx.body = news;
+
 });
 
-
-//app.use(json());
 app.use(router.routes());
-
-
-router.get('/test', ctx => (ctx.body = 'Hello from Test'));
-
-
 
 app.listen(3000, () => console.log('Server is listening...'));
