ad84a7d52bff73bd73761d269532e56c8f4c909d
ferricode <ferided88@gmail.com>
2023-03-16 23:15:48 +0200

    Modified newsService.

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -2,4 +2,3 @@ const Publication = require('../models/Publication');
 
 exports.getOne = (publicationId) => Publication.findById(publicationId);
 exports.getAll = () => Publication.find();
-exports.filterDate = (startDate, endDate) => Publication.find({}).where('publishedAt').gt(startDate).lt(endDate);
